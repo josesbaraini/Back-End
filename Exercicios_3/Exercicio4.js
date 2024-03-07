5c5942e48d033cba0ad812db87631fd574c3cca9
José <josesbarainips@gmail.com>
2024-03-07 16:45:09 -0400

    Exercicio_4

--- Exercicios_3/Exercicio4.js
+++ Exercicios_3/Exercicio4.js
@@ -3,29 +3,30 @@ import entraDados from "readline-sync";
 let categoria = entraDados.question("Informe o sua categoria: ")
 let salario = entraDados.questionFloat("Informe o seu Salario: ")
 
-if (categoria="A") {
-    console.log(salario*1.05)
-    
+
+switch (categoria) {
+    case "A":
+        console.log(salario*1.05)
+
+        break;
+    case "B":
+        console.log(salario*1.1)
+
+        break;
+    case "C":
+        console.log(salario*1.15)
+
+
+        break;
+    case "D":
+        console.log(salario*1.2)
+
+
+        break;
+
+
+    default:
+
+    console.log("Informações invalidas")
+        break;
 }
-else if (categoria="B") {
-    console.log(salario*1.1)
-    
-}
-
-
-else if (categoria="C") {
-    console.log(salario*1.15)
-    
-}
-
-else if (categoria="D") {
-    console.log(salario*1.20)
-    
-}
-
-
-else {
-    console.log("Dados invalidos inseridos")
-    
-}
-
