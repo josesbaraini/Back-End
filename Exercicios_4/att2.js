e751a99bdc6c10b17239e3fc4f49aec676e4db9e
José <josesbarainips@gmail.com>
2024-03-14 16:31:04 -0400

    atualização atividade 2

--- Exercicios_4/att2.js
+++ Exercicios_4/att2.js
@@ -3,24 +3,24 @@ import entraDados from "readline-sync"
 let num_alunos = entraDados.questionFloat("Digite o numero de alunos")
 let cont_alunos = 0
 let aluno_nota = 0
-let alunomf=[]
+let alunomf=0
 let alunonat=1
 
 while (cont_alunos<num_alunos) {
     while (alunonat<5){
-        let notaa = entraDados.questionFloat("Digite a nota do aluno:")
+        let notaa = entraDados.questionFloat(`Digite a ${alunonat} nota do ${cont_alunos+1} aluno:`)
             aluno_nota+=notaa
             alunonat++
         
 
     }
-    console.log(`nota do aluno: ${aluno_nota/4}`)
+    console.log(`Média do ${cont_alunos+1} aluno: ${aluno_nota/4}`)
 
-    alunomf=+aluno_nota/4
+    alunomf=(aluno_nota/4)+alunomf
     alunonat=1
     aluno_nota=0
     cont_alunos++
 }
 
 
-console.log(`Nota da turma: ${alunomf/num_alunos}`)
+console.log(`Média da turma: ${alunomf/num_alunos}`)
