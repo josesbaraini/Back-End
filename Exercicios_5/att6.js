eba0004a08d4574b8e85f451218c8ecee8586de9
José <josesbarainips@gmail.com>
2024-03-28 16:54:51 -0400

    att 6

--- Exercicios_5/att6.js
+++ Exercicios_5/att6.js
@@ -4,15 +4,15 @@ let raio =rl.questionInt("Digite um raio")
 const pi=3.14159
 
 function calcularArea1(raio) {
-    return ((raio)=> pi*(raio*raio)).ToFixed(2)
+    return pi*(raio*raio)
     
 }
 
 const calcularArea2= function (raio) {
-    return ((raio)=> pi*(raio*raio)).ToFixed(2)
+    return (pi*(raio*raio))
     
 }
 
-const calcularArea3 = ((raio)=> pi*(raio*raio)).ToFixed(2)
+const calcularArea3 = ((raio)=> pi*(raio*raio))
 
-console.log(calcularArea1(raio),calcularArea2(raio),calcularArea3(raio))
+console.log(calcularArea1(raio).toFixed(2),calcularArea2(raio).toFixed(2),calcularArea3(raio).toFixed(2))
