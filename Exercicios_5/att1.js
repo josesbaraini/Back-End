5934be836f83af89c53461405e027e4f1d7d30ee
José <josesbarainips@gmail.com>
2024-03-28 16:21:34 -0400

    att

---
+++ Exercicios_5/att1.js
@@ -0,0 +1,20 @@
+import rl from "readline-sync";
+
+let string =rl.question("Digite uma string")
+
+function paraAlta(a){
+    return a.toUpperCase() 
+}
+
+
+const praAlta= function(a){
+    return a.toUpperCase()
+
+}
+
+const aAlta = (a) => a.toUpperCase()
+
+
+console.log(aAlta(string),praAlta(string),paraAlta(string))
+
+
---
+++ Exercicios_5/att2.js
@@ -0,0 +1,10 @@
+import rl from "readline-sync";
+
+let numero =rl.questionInt("Digite um numero")
+
+function name(numero) {
+    if (numero%2==2) {
+        return""Par
+    }
+    
+}
