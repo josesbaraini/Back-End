7c5eff8899c7f870aa9c6b42836a19f6ab864d86
José <josesbarainips@gmail.com>
2024-03-28 16:46:02 -0400

    atividade 5

---
+++ Exercicios_5/att5.js
@@ -0,0 +1,19 @@
+import rl from "readline-sync";
+
+let numero =rl.questionInt("Digite um numero")
+let porcentagem =rl.questionInt("Digite uma porcentagem")
+
+function porsentagem(numero,porcentagem) {
+    return numero-(numero*((porcentagem/100)))
+    
+}
+
+
+const testes = function (numero,porcentagem) {
+    return numero-(numero*((porcentagem/100)))
+    
+}
+
+const numeros = (numero,porcentagem)=>numero-(numero*((porcentagem/100)))
+
+console.log(porsentagem(numero,porcentagem),testes(numero,porcentagem),numeros(numero,porcentagem))
